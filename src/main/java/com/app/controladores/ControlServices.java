f7c4611ab0c59e7b7ee98f6d3e37f9c806f8bec3
jdhernandez <jdhernandez@indracompany.com>
2023-03-15 17:02:57 -0500

    Ajuste para validacion de token

--- src/main/java/com/app/controladores/ControlServices.java
+++ src/main/java/com/app/controladores/ControlServices.java
@@ -1,13 +1,26 @@
 package com.app.controladores;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.app.modelos.Usuario;
+import com.app.servicios.UsuarioServiceImpl;
 
 @RestController
 public class ControlServices {
+	@Autowired
+	private UsuarioServiceImpl UsuarioServices;
 
 	@GetMapping("ping")
 	public String ping() {
 		return "Pong!";
+	}
+
+	@GetMapping("token/habilita/{user}")
+	public Usuario TokenVerify(@PathVariable String user) {
+
+		return UsuarioServices.usuarioPorUser(user);
 	}
 }
